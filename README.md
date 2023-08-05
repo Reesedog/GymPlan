e098d7adfbebb182b699fcbe95971ab1547adc7b
Reesedog <Reesedog>
2023-08-05 03:22:26 +0000

    now records can be deleted individually

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 Hi
+This is a web that record and track my gym plans.
+The plan is to cycle on a weekly basis. 
+Excercice can be stored with set number and weight.
+See u at the gym :）
--- gymRecord.js
+++ gymRecord.js
@@ -1,50 +1,54 @@
 let gymRecords = {
-    Monday: [],
-    Tuesday: [],
-    Wednesday: [],
-    Thursday: [],
-    Friday: [],
-    Saturday: [],
-    Sunday: []
-  };
-  
-  function addRecord(day) {
-    let exercise = prompt("Enter the exercise:");
-    let sets = prompt("Enter the number of sets:");
-    let weight = prompt("Enter the weight:");
-  
-    let record = {
-      exercise: exercise,
-      sets: sets,
-      weight: weight
+  Monday: [],
+  Tuesday: [],
+  Wednesday: [],
+  Thursday: [],
+  Friday: [],
+  Saturday: [],
+  Sunday: []
+};
+
+function init() {
+  for (let day in gymRecords) {
+    displayRecords(day);
+  }
+}
+
+function displayRecords(day) {
+  const ul = document.getElementById(`${day}-records`);
+  ul.innerHTML = '';  
+
+  gymRecords[day].forEach((record, index) => {
+    const li = document.createElement('li');
+    li.textContent = `${record.exercise}, Sets: ${record.sets}, Weight: ${record.weight}`;
+
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Delete';
+    deleteButton.className = 'delete';  
+    deleteButton.onclick = function () {
+      deleteRecord(day, index);
     };
-  
-    gymRecords[day].push(record);
-    displayRecords(day);
-  }
-  
-  function deleteRecord(day) {
-    gymRecords[day].pop();
-    displayRecords(day);
-  }
-  
-  function displayRecords(day) {
-    let dayDiv = document.getElementById(day + "-records");
-    dayDiv.innerHTML = "";
-  
-    for (let record of gymRecords[day]) {
-      let p = document.createElement("p");
-      p.textContent = "Exercise: " + record.exercise + ", Sets: " + record.sets + ", Weight: " + record.weight;
-      dayDiv.appendChild(p);
-    }
-  }
-  
-  // Call displayRecords for each day to show record
-  displayRecords('Monday');
-  displayRecords('Tuesday');
-  displayRecords('Wednesday');
-  displayRecords('Thursday');
-  displayRecords('Friday');
-  displayRecords('Saturday');
-  displayRecords('Sunday');
-  
+    li.appendChild(deleteButton);
+
+    ul.appendChild(li);
+  });
+}
+
+function addRecord(day) {
+  let exercise = prompt("Enter the exercise:");
+  let sets = prompt("Enter the number of sets:");
+  let weight = prompt("Enter the weight:");
+  const record = {
+    exercise: exercise,
+    sets: sets,
+    weight: weight
+  };
+
+  gymRecords[day].push(record);
+  displayRecords(day);
+}
+
+function deleteRecord(day, index) {
+  gymRecords[day].splice(index, 1);
+  displayRecords(day);
+}
